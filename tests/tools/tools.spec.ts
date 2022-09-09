419744ff084577a5a3bcf7b2b4cead54e927d7e4
Guillaume Masclet <guillaume.masclet@linkurio.us>
2022-09-09 14:55:27 +0200

    LKE-7037: Format monetary values in captions (#249)
    * LKE-7037: Format monetary values in captions
    
    * LKE-7037: Export Tools

---
+++ tests/tools/tools.spec.ts
@@ -0,0 +1,168 @@
+import {expect} from 'chai';
+import 'mocha';
+import {CurrencyFormat, ICurrencyOptions} from '@linkurious/rest-client';
+
+import {Tools} from '../../src/tools/tools';
+
+describe('Tools', () => {
+  describe('Tools.formatCurrencyValue', () => {
+    describe('Format: "[Symbol] #,###.##"', () => {
+      const options: ICurrencyOptions = {
+        type: 'currency',
+        format: CurrencyFormat.SYMBOL_COMMAS_DOT,
+        symbol: '$'
+      };
+
+      it('should format NaN', () => {
+        const result = Tools.formatCurrencyValue(NaN, options);
+        expect(result).eql('$ NaN');
+      });
+      it('should format a single digit value', () => {
+        const result = Tools.formatCurrencyValue(9, options);
+        expect(result).eql('$ 9.00');
+      });
+      it('should format a 3 digit value', () => {
+        const result = Tools.formatCurrencyValue(123, options);
+        expect(result).eql('$ 123.00');
+      });
+      it('should format a 4 digit value', () => {
+        const result = Tools.formatCurrencyValue(4123, options);
+        expect(result).eql('$ 4,123.00');
+      });
+      it('should format a 7 digit value', () => {
+        const result = Tools.formatCurrencyValue(7456123, options);
+        expect(result).eql('$ 7,456,123.00');
+      });
+      it('should format a negative value', () => {
+        const result = Tools.formatCurrencyValue(-7456123, options);
+        expect(result).eql('- $ 7,456,123.00');
+      });
+      it('should format a decimal value', () => {
+        const result = Tools.formatCurrencyValue(7456123.89, options);
+        expect(result).eql('$ 7,456,123.89');
+      });
+      it('should format a decimal value rounding up', () => {
+        const result = Tools.formatCurrencyValue(7456123.897, options);
+        expect(result).eql('$ 7,456,123.90');
+      });
+      it('should format a negative decimal value rounding down', () => {
+        const result = Tools.formatCurrencyValue(-7456123.892, options);
+        expect(result).eql('- $ 7,456,123.89');
+      });
+      it('should format min safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MIN_SAFE_INTEGER, options);
+        expect(result).eql('- $ 9,007,199,254,740,991.00');
+      });
+      it('should format max safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MAX_SAFE_INTEGER, options);
+        expect(result).eql('$ 9,007,199,254,740,991.00');
+      });
+    });
+
+    describe('Format: "#.###,## [Symbol]"', () => {
+      const options: ICurrencyOptions = {
+        type: 'currency',
+        format: CurrencyFormat.DOTS_COMMA_SYMBOL,
+        symbol: '$'
+      };
+
+      it('should format NaN', () => {
+        const result = Tools.formatCurrencyValue(NaN, options);
+        expect(result).eql('NaN $');
+      });
+      it('should format a single digit value', () => {
+        const result = Tools.formatCurrencyValue(9, options);
+        expect(result).eql('9,00 $');
+      });
+      it('should format a 3 digit value', () => {
+        const result = Tools.formatCurrencyValue(123, options);
+        expect(result).eql('123,00 $');
+      });
+      it('should format a 4 digit value', () => {
+        const result = Tools.formatCurrencyValue(4123, options);
+        expect(result).eql('4.123,00 $');
+      });
+      it('should format a 7 digit value', () => {
+        const result = Tools.formatCurrencyValue(7456123, options);
+        expect(result).eql('7.456.123,00 $');
+      });
+      it('should format a negative value', () => {
+        const result = Tools.formatCurrencyValue(-7456123, options);
+        expect(result).eql('- 7.456.123,00 $');
+      });
+      it('should format a decimal value', () => {
+        const result = Tools.formatCurrencyValue(7456123.89, options);
+        expect(result).eql('7.456.123,89 $');
+      });
+      it('should format a decimal value rounding up', () => {
+        const result = Tools.formatCurrencyValue(7456123.897, options);
+        expect(result).eql('7.456.123,90 $');
+      });
+      it('should format a negative decimal value rounding down', () => {
+        const result = Tools.formatCurrencyValue(-7456123.892, options);
+        expect(result).eql('- 7.456.123,89 $');
+      });
+      it('should format min safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MIN_SAFE_INTEGER, options);
+        expect(result).eql('- 9.007.199.254.740.991,00 $');
+      });
+      it('should format max safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MAX_SAFE_INTEGER, options);
+        expect(result).eql('9.007.199.254.740.991,00 $');
+      });
+    });
+
+    describe('Format: "# ###,## [Symbol]"', () => {
+      const options: ICurrencyOptions = {
+        type: 'currency',
+        format: CurrencyFormat.SPACES_COMMA_DOT,
+        symbol: '$'
+      };
+
+      it('should format NaN', () => {
+        const result = Tools.formatCurrencyValue(NaN, options);
+        expect(result).eql('NaN $');
+      });
+      it('should format a single digit value', () => {
+        const result = Tools.formatCurrencyValue(9, options);
+        expect(result).eql('9,00 $');
+      });
+      it('should format a 3 digit value', () => {
+        const result = Tools.formatCurrencyValue(123, options);
+        expect(result).eql('123,00 $');
+      });
+      it('should format a 4 digit value', () => {
+        const result = Tools.formatCurrencyValue(4123, options);
+        expect(result).eql('4 123,00 $');
+      });
+      it('should format a 7 digit value', () => {
+        const result = Tools.formatCurrencyValue(7456123, options);
+        expect(result).eql('7 456 123,00 $');
+      });
+      it('should format a negative value', () => {
+        const result = Tools.formatCurrencyValue(-7456123, options);
+        expect(result).eql('- 7 456 123,00 $');
+      });
+      it('should format a decimal value', () => {
+        const result = Tools.formatCurrencyValue(7456123.89, options);
+        expect(result).eql('7 456 123,89 $');
+      });
+      it('should format a decimal value rounding up', () => {
+        const result = Tools.formatCurrencyValue(7456123.897, options);
+        expect(result).eql('7 456 123,90 $');
+      });
+      it('should format a negative decimal value rounding down', () => {
+        const result = Tools.formatCurrencyValue(-7456123.892, options);
+        expect(result).eql('- 7 456 123,89 $');
+      });
+      it('should format min safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MIN_SAFE_INTEGER, options);
+        expect(result).eql('- 9 007 199 254 740 991,00 $');
+      });
+      it('should format max safe integer', () => {
+        const result = Tools.formatCurrencyValue(Number.MAX_SAFE_INTEGER, options);
+        expect(result).eql('9 007 199 254 740 991,00 $');
+      });
+    });
+  });
+});
