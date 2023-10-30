24c904a1a364dbd5704c506d7ee676e3cb34b990
aminerhanemi <amine@linkurio.us>
2023-10-30 19:31:05 +0100

    Update transformations.ts

--- src/ogma/features/transformations.ts
+++ src/ogma/features/transformations.ts
@@ -1,9 +1,10 @@
 'use strict';
 
-import {GenericObject, IEdgeGroupStyle, LkEdgeData, LkNodeData} from '@linkurious/rest-client';
+import { GenericObject, IEdgeGroupStyle, LkEdgeData, LkNodeData, LkProperty } from "@linkurious/rest-client";
 import {
   Edge,
   EdgeExtremity,
+  EdgeList,
   EdgeStyle,
   EdgeType,
   PixelSize,
@@ -12,6 +13,7 @@ import {
 } from '@linkurious/ogma';
 
 import {LKOgma} from '../index';
+import {Tools} from '../../tools/tools';
 
 const DEFAULT_EDGE_GROUP_STYLE: {
   color: string;
@@ -58,7 +60,8 @@ export class TransformationsViz {
           return {
             data: {
               properties: {
-                originalType: edges.getData('type')[0]
+                originalType: edges.getData('type')[0],
+                ...this.getGroupedEdgeProperties(edges)
               }
             }
           };
@@ -67,6 +70,65 @@ export class TransformationsViz {
     } else {
       await this.transformation.refresh();
     }
+  }
+
+  private getGroupedEdgeProperties(
+    edges: EdgeList<LkEdgeData, LkNodeData>
+  ): Record<string, unknown> {
+    const propertiesMap = new Map<
+      string,
+      {
+        values: Array<LkProperty>;
+        aggregatedNumber?: {min: number; max: number};
+        aggregatedString?: {countDistinct: number};
+      }
+    >();
+    edges.getData('properties').forEach((properties) => {
+      Object.keys(properties).forEach((key) => {
+        const isNumber = Tools.isNumber(properties[key]);
+        // check if property does not exist  in propertiesMap
+        if (!propertiesMap.has(key)) {
+          if (isNumber) {
+            propertiesMap.set(key, {
+              values: [properties[key]],
+              aggregatedNumber: {max: properties[key] as number, min: properties[key] as number}
+            });
+          } else {
+            propertiesMap.set(key, {
+              values: [properties[key]],
+              aggregatedString: {countDistinct: 1}
+            });
+          }
+        } else {
+          const currentProperty = propertiesMap.get(key)!;
+          if (isNumber) {
+            const aggregatedNumber: {min: number; max: number} = currentProperty?.aggregatedNumber!;
+            aggregatedNumber.max = Math.max(aggregatedNumber.max, properties[key] as number);
+            aggregatedNumber.min = Math.min(aggregatedNumber.min, properties[key] as number);
+            currentProperty.values.push(properties[key]);
+          } else {
+            const aggregatedString = currentProperty.aggregatedString!;
+            if (!currentProperty.values.includes(properties[key])) {
+              aggregatedString.countDistinct++;
+            }
+            currentProperty.values.push(properties[key]);
+          }
+        }
+      });
+    });
+    const properties: Record<string, unknown> = {};
+    propertiesMap.forEach(function (value, key) {
+      properties['propertyName'] = key;
+      if (value.aggregatedNumber !== undefined) {
+        properties[`${key}_Max`] = value.aggregatedNumber.max;
+        properties[`${key}_Min`] = value.aggregatedNumber.min;
+      }
+      if (value.aggregatedString !== undefined) {
+        properties[`${key}_CountDistinct`] = value.aggregatedString.countDistinct;
+      }
+      properties[`${key}_Values`] = value.values;
+    });
+    return properties;
   }
 
   /**
