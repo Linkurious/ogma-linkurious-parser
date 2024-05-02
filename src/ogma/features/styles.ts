01b86edda154b471d8572d3c4aa5ec6bb3ce1825
aminerhanemi <amine@linkurio.us>
2024-05-02 16:47:42 +0200

    define the scale of the badge

--- src/ogma/features/styles.ts
+++ src/ogma/features/styles.ts
@@ -523,9 +523,14 @@ export class StylesViz {
               const textColor = OgmaTools.isBright(nodeColor as o.Color)
                 ? DARK_FONT_COLOR
                 : CLEAR_FONT_COLOR;
+              const MAX = 5;
+              const defaultRatio = 1 / 5;
+              const radius = node.getAttribute('radius') as number;
+              const scale = radius * defaultRatio > MAX ? MAX/radius : defaultRatio;
               return {
                 color: 'inherit',
                 minVisibleSize: 20,
+                scale: scale,
                 stroke: {
                   width: 0,
                   color: null
