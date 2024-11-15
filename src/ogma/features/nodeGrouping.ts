2b1433669c784b42e4cf6e5455f126a1891091c8
Alexander Milevski <alexander@linkurio.us>
2024-11-15 13:44:05 +0100

    LKE-12367 Smart layouts in the group nodes (#424)
    * Smart layouts in the group nodes
    
    Added more options to subnodes layout to make it faster
    
    * More versatile options
    
    * Refactored to contain everything in one file
    
    * Moved 2 nodes layout into a method
    
    * Same for the chain layout
    
    * Use topological sort from OgmaTool
    
    * Put all the special layouts in node grouping
    
    * Fixed build

--- src/ogma/features/nodeGrouping.ts
+++ src/ogma/features/nodeGrouping.ts
@@ -1,4 +1,4 @@
-import {Transformation, Node, NodeList, StyleRule} from '@linkurious/ogma';
+import {Transformation, Node, NodeList, StyleRule, PixelSize, Point} from '@linkurious/ogma';
 import {
   IVizNodeGroupInfo,
   LkEdgeData,
@@ -10,9 +10,20 @@ import sha1 from 'sha1';
 
 import {FORCE_LAYOUT_CONFIG, LKOgma} from '../index';
 import {Tools} from '../../tools/tools';
+import {OgmaTools} from '../../tools/ogmaTool';
 
 export const LKE_NODE_GROUPING_EDGE = 'LKE_NODE_GROUPING_EDGE';
 export const LKE_NODE_GROUPING_NODE = 'LKE_NODE_GROUPING_NODE';
+
+interface CircularLayoutOptions {
+  radii: PixelSize[] | number[];
+  cx?: number;
+  cy?: number;
+  startAngle?: number;
+  clockwise?: boolean;
+  getRadius?: (radius: PixelSize) => number;
+  distanceRatio?: number;
+}
 
 export class NodeGroupingTransformation {
   public transformation?: Transformation<LkNodeData, LkEdgeData>;
@@ -147,12 +158,37 @@ export class NodeGroupingTransformation {
       return;
     }
 
-    const noEdges = subNodes.getAdjacentEdges({bothExtremities: true}).size === 0;
-    if (noEdges) {
-      await this._runCirclePack(subNodes);
-    } else {
-      await this._runForceLayout(subNodes);
+    // 2 nodes
+    if (subNodes.size === 2) {
+      await this._runTwoNodesLayout(subNodes);
+      return;
     }
+
+    const noEdges = subNodes.getAdjacentEdges({bothExtremities: true}).size === 0;
+    if (noEdges) return this._runCirclePack(subNodes);
+    // stars
+    const center = this.isStar(subNodes);
+    if (center) {
+      const satellites = subNodes.filter((n) => n !== center);
+      const positions = this._runCircularLayout({
+        radii: satellites.getAttribute('radius'),
+        cx: center.getAttribute('x'),
+        cy: center.getAttribute('y'),
+        clockwise: false,
+        distanceRatio: 5
+      });
+      const list = center.toList().concat(satellites);
+      await list.setAttributes([center.getPosition(), ...positions]);
+      return;
+    }
+    // Chains: if al nodes have degree 1 or 2, place them in a line
+    const degrees = subNodes.getDegree();
+    if (degrees.every((d) => d > 0 && d <= 2)) {
+      await this._runChainLayout(subNodes);
+      return;
+    }
+
+    return await this._runForceLayout(subNodes);
   }
 
   /**
@@ -210,6 +246,18 @@ export class NodeGroupingTransformation {
       nodes: subNodes,
       margin: 10,
       sort: 'asc'
+    });
+  }
+
+  private async _runChainLayout(subNodes: NodeList<LkNodeData, LkEdgeData>): Promise<void> {
+    // straighten the chain
+    const sortedNodes = this._ogma.getNodes(OgmaTools.topologicalSort(subNodes));
+    // we also need to sort the nodes so that they are following the chain
+    await this._ogma.layouts.grid({
+      nodes: sortedNodes,
+      // TODO: test that visually
+      colDistance: Math.max(...subNodes.getAttribute('radius').map(Number)) * 4,
+      rows: 1
     });
   }
 
@@ -281,5 +329,64 @@ export class NodeGroupingTransformation {
         '-'
       )}-${propertyValue}`
     );
+  }
+
+  public _runCircularLayout({
+    radii,
+    clockwise = true,
+    cx = 0,
+    cy = 0,
+    startAngle = (3 / 2) * Math.PI,
+    getRadius = (radius: PixelSize) => Number(radius),
+    distanceRatio = 0.0
+  }: CircularLayoutOptions): Point[] {
+    const N = radii.length;
+    // dummy checks
+    if (N === 0) return [];
+    if (N === 1) return [{x: cx, y: cy}];
+
+    // minDistance
+    const minDistance =
+      radii.map(getRadius).reduce((acc, r) => Math.max(acc, r), 0) * (2 + distanceRatio);
+
+    const sweep = 2 * Math.PI - (2 * Math.PI) / N;
+    const deltaAngle = sweep / Math.max(1, N - 1);
+
+    const dcos = Math.cos(deltaAngle) - Math.cos(0);
+    const dsin = Math.sin(deltaAngle) - Math.sin(0);
+
+    const rMin = Math.sqrt((minDistance * minDistance) / (dcos * dcos + dsin * dsin));
+    const r = Math.max(rMin, 0);
+
+    return radii.map((_, i) => {
+      const angle = startAngle + i * deltaAngle * (clockwise ? 1 : -1);
+
+      const rx = r * Math.cos(angle);
+      const ry = r * Math.sin(angle);
+      return {
+        x: cx + rx,
+        y: cy + ry
+      };
+    });
+  }
+
+  private async _runTwoNodesLayout(nodes: NodeList<LkNodeData, LkEdgeData>) {
+    const radii = nodes.getAttribute('radius').map(Number);
+    const positions = nodes.getPosition();
+    const gap = Math.min(...radii);
+    await nodes.setAttributes([
+      positions[0],
+      {x: positions[0].x + gap + radii[0] + radii[1], y: positions[0].y}
+    ]);
+  }
+
+  private isStar(nodes: NodeList) {
+    for (const id of nodes.getId()) {
+      const node = this._ogma.getNode(id)!;
+      const adjacent = node.getAdjacentNodes();
+      const isStar = node.getDegree() > 2 && adjacent.getDegree().every((d) => d === 1);
+      if (isStar && adjacent.size + 1 === nodes.size) return node;
+    }
+    return false;
   }
 }
--- src/tools/ogmaTool.ts
+++ src/tools/ogmaTool.ts
@@ -1,5 +1,5 @@
 'use strict';
-import {Color, NodeList, Node, EdgeList, Edge} from '@linkurious/ogma';
+import {Color, NodeList, Node, EdgeList, Edge, NodeId} from '@linkurious/ogma';
 import {LkEdgeData, LkNodeData} from '@linkurious/rest-client';
 
 import {Tools} from './tools';
@@ -94,5 +94,32 @@ export class OgmaTools {
     items: NodeList<LkNodeData, LkEdgeData> | EdgeList<LkEdgeData, LkNodeData>
   ): items is NodeList<LkNodeData, LkEdgeData> {
     return items.isNode;
+  }
+
+  public static topologicalSort(nodes: NodeList): NodeId[] {
+    const nodesArray = nodes.toArray();
+    let currentNode: Node | null = nodesArray.find((n) => n.getDegree() === 1)!;
+    const visited = new Set();
+    const stack: Node[] = [];
+    while (currentNode) {
+      stack.push(currentNode);
+      visited.add(currentNode);
+
+      const nextNode = currentNode
+        .getAdjacentNodes()
+        .filter((neighbor) => !visited.has(neighbor))
+        .get(0);
+      currentNode = nextNode === undefined ? null : nextNode;
+    }
+    return stack.map((n) => n.getId());
+  }
+
+  public static isStar(nodes: NodeList) {
+    for (const node of nodes.toArray()) {
+      const adjacent = node.getAdjacentNodes();
+      const isStar = node.getDegree() > 2 && adjacent.getDegree().every((d) => d === 1);
+      if (isStar && adjacent.size + 1 === nodes.size) return node;
+    }
+    return false;
   }
 }
